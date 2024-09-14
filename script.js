92b648ed1db6558ffea7fff0d6ba1f0aed21e0ee
Tristan de Jong <trisdejong777@gmail.com>
2024-09-15 00:47:08 +0200

    commit

--- script.js
+++ script.js
@@ -29,17 +29,17 @@ document.addEventListener('DOMContentLoaded', function() {
     
         // Voeg hier je embedded links toe
         const images = [
-            'https://drive.google.com/file/d/1-hq9MRwPafc2Do7J9OY05GchbMex_Tnr/view?usp=sharing',
-            'https://drive.google.com/file/d/1-Wgjup67vX9ntoOOvqMpZWzZ4Li1yL-Z/view?usp=sharing',
-            'https://drive.google.com/file/d/1-RLHEV2dNijkRh5Si_Cu8Qu03VJkp3aY/view?usp=sharing',
-            'https://drive.google.com/file/d/1-dLSwSNJ_-FS-yI-yKF26YQci379cgwx/view?usp=sharing',
-            'https://drive.google.com/file/d/1-L6zcoNsn7MbHAw0w7-y4lJx2xkIOs9P/view?usp=sharing',
-            'https://drive.google.com/file/d/1-LfWJY9eeTe9TyLF9Em6UNLkVdT9Jo6G/view?usp=sharing',
-            'https://drive.google.com/file/d/19Xww4qarS3JfUzQvsye7lSxq5E9d8bya/view?usp=sharing',
-            'https://drive.google.com/file/d/19crEsAEP5bQvFZWXCsix8h1jPjgRnPQ0/view?usp=sharing',
-            'https://drive.google.com/file/d/19llb8fne02xVKTO90ErEezVjvotGa4fG/view?usp=sharing',
-            'https://drive.google.com/file/d/19xQJ1UskD9IZMnPCRTLPHIZvsvH2oh8p/view?usp=sharing',
-            'https://drive.google.com/file/d/19tFwiLCfJFrSF_Gd-CV-JFPDHlIThj1W/view?usp=sharing',
+            'https://drive.google.com/file/d/1-hq9MRwPafc2Do7J9OY05GchbMex_Tnr/view?usp=sharing.png',
+            'https://drive.google.com/file/d/1-Wgjup67vX9ntoOOvqMpZWzZ4Li1yL-Z/view?usp=sharing.png',
+            'https://drive.google.com/file/d/1-RLHEV2dNijkRh5Si_Cu8Qu03VJkp3aY/view?usp=sharing.png',
+            'https://drive.google.com/file/d/1-dLSwSNJ_-FS-yI-yKF26YQci379cgwx/view?usp=sharing.png',
+            'https://drive.google.com/file/d/1-L6zcoNsn7MbHAw0w7-y4lJx2xkIOs9P/view?usp=sharing.png',
+            'https://drive.google.com/file/d/1-LfWJY9eeTe9TyLF9Em6UNLkVdT9Jo6G/view?usp=sharing.png',
+            'https://drive.google.com/file/d/19Xww4qarS3JfUzQvsye7lSxq5E9d8bya/view?usp=sharing.png',
+            'https://drive.google.com/file/d/19crEsAEP5bQvFZWXCsix8h1jPjgRnPQ0/view?usp=sharing.png',
+            'https://drive.google.com/file/d/19llb8fne02xVKTO90ErEezVjvotGa4fG/view?usp=sharing.png',
+            'https://drive.google.com/file/d/19xQJ1UskD9IZMnPCRTLPHIZvsvH2oh8p/view?usp=sharing.png',
+            'https://drive.google.com/file/d/19tFwiLCfJFrSF_Gd-CV-JFPDHlIThj1W/view?usp=sharing.png',
             // Voeg meer links toe als je wilt 'hier je link',
         ];
     
